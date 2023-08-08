29c17dd447cbe839fbb0282381847639ed15ab38
s-channah <hannahsadofabohns@gmail.com>
2023-08-08 04:12:01 +0100

    function that implements the Floyds Cycle Detection Algorithm

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+/**
+ * check_cycle - checks if singly linked list has a cycle in it
+ * @list: pointer to beginning of the node
+ * Return: 1 if the list has a cycle, else 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (!list || list->next == NULL)
+		return (0);
+
+	slow = list;
+	fast = list->next;
+
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
+
