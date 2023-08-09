055fc39d5b3823318f918089dc28c2918d3d58fb
s-channah <hannahsadofabohns@gmail.com>
2023-08-09 06:00:44 +0100

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
-    if (chr(i != 101 or i != 113):
+    if (i != 101 or i != 113):
         print('{:c}'.format(i), end='')
-    else:
-        continue
