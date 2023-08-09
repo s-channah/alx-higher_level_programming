f94b6480410848c7500e5d0d5623bee63080342f
s-channah <hannahsadofabohns@gmail.com>
2023-08-09 05:56:39 +0100

    task 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 for i in range(97, 123):
-    if (chr(i) != 'q' or chr(i) != 'e'):
+    if (chr(i != 101 or i != 113):
         print('{:c}'.format(i), end='')
     else:
         continue
